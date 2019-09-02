5d3a07f685d6b021cd38a9c7d5beccfaf88f1efa
alvas <qingyun.oracle@gmail.com>
2019-09-02 01:09:54 -0700

    add MonotoneChain.cpp

--- geom/MonotoneChain.cpp
+++ geom/MonotoneChain.cpp
@@ -1,103 +1,96 @@
 // Implementation of Andrew's monotone chain 2D convex hull algorithm.
-//
-// // Asymptotic complexity: O(n log n).
-//
-// // Practical performance: 0.5-1.0 seconds for n=1000000 on a 1GHz machine.
-//
-// #include <algorithm>
-//
-// #include <vector>
-//
-// using namespace std;
-//
-//
-//
-// typedef double coord_t;         // coordinate type
-//
-// typedef double coord2_t;  // must be big enough to hold 2*max(|coordinate|)^2
-//
-//
-//
-// struct Point {
-//
-// 	coord_t x, y;
-//
-//
-//
-// 		bool operator <(const Point &p) const {
-//
-// 				return x < p.x || (x == p.x && y < p.y);
-//
-// 					}
-//
-// 					};
-//
-//
-//
-// 					// 3D cross product of OA and OB vectors, (i.e z-component of their "2D" cross product, but remember that it is not defined in "2D").
-//
-// 					// Returns a positive value, if OAB makes a counter-clockwise turn,
-//
-// 					// negative for clockwise turn, and zero if the points are collinear.
-//
-// 					coord2_t cross(const Point &O, const Point &A, const Point &B)
-//
-// 					{
-//
-// 						return (A.x - O.x) * (B.y - O.y) - (A.y - O.y) * (B.x - O.x);
-//
-// 						}
-//
-//
-//
-// 						// Returns a list of points on the convex hull in counter-clockwise order.
-//
-// 						// Note: the last point in the returned list is the same as the first one.
-//
-// 						vector<Point> convex_hull(vector<Point> P)
-//
-// 						{
-//
-// 							size_t n = P.size(), k = 0;
-//
-// 								if (n <= 3) return P;
-//
-// 									vector<Point> H(2*n);
-//
-//
-//
-// 										// Sort points lexicographically
-//
-// 											sort(P.begin(), P.end());
-//
-//
-//
-// 												// Build lower hull
-//
-// 													for (size_t i = 0; i < n; ++i) {
-//
-// 															while (k >= 2 && cross(H[k-2], H[k-1], P[i]) <= 0) k--;
-//
-// 																	H[k++] = P[i];
-//
-// 																		}
-//
-//
-//
-// 																			// Build upper hull
-//
-// 																				for (size_t i = n-1, t = k+1; i > 0; --i) {
-//
-// 																						while (k >= t && cross(H[k-2], H[k-1], P[i-1]) <= 0) k--;
-//
-// 																								H[k++] = P[i-1];
-//
-// 																									}
-//
-//
-//
-// 																										H.resize(k-1);
-//
-// 																											return H;
-//
-// 																											}
+
+// Asymptotic complexity: O(n log n).
+
+// Practical performance: 0.5-1.0 seconds for n=1000000 on a 1GHz machine.
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+typedef double coord_t;         // coordinate type
+
+typedef double coord2_t;  // must be big enough to hold 2*max(|coordinate|)^2
+
+struct Point {
+
+    coord_t x, y;
+
+    bool operator <(const Point &p) const {
+
+        return x < p.x || (x == p.x && y < p.y);
+
+    }
+
+};
+
+
+
+// 3D cross product of OA and OB vectors, (i.e z-component of their "2D" cross product, but remember that it is not defined in "2D").
+
+// Returns a positive value, if OAB makes a counter-clockwise turn,
+
+// negative for clockwise turn, and zero if the points are collinear.
+
+coord2_t cross(const Point &O, const Point &A, const Point &B)
+
+{
+
+    return (A.x - O.x) * (B.y - O.y) - (A.y - O.y) * (B.x - O.x);
+
+}
+
+
+
+// Returns a list of points on the convex hull in counter-clockwise order.
+
+// Note: the last point in the returned list is the same as the first one.
+
+vector<Point> convex_hull(vector<Point> P)
+
+{
+
+    size_t n = P.size(), k = 0;
+
+    if (n <= 3) return P;
+
+    vector<Point> H(2*n);
+
+
+
+    // Sort points lexicographically
+
+    sort(P.begin(), P.end());
+
+
+
+    // Build lower hull
+
+    for (size_t i = 0; i < n; ++i) {
+
+        while (k >= 2 && cross(H[k-2], H[k-1], P[i]) <= 0) k--;
+
+        H[k++] = P[i];
+
+    }
+
+
+
+    // Build upper hull
+
+    for (size_t i = n-1, t = k+1; i > 0; --i) {
+
+        while (k >= t && cross(H[k-2], H[k-1], P[i-1]) <= 0) k--;
+
+        H[k++] = P[i-1];
+
+    }
+
+
+
+    H.resize(k-1);
+
+    return H;
+
+}
